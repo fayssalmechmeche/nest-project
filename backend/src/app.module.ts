305778e8ff0e5b2c6bea292cb900c64d6d7b29e2
Fayssal Mechmeche <fayssalmechmeche.pro@gmail.com>
2025-05-23 10:06:06 +0200

    Update @types/node to version 22.15.21, remove Rooms module and related files, and add environment variables for frontend configuration.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,7 +3,6 @@ import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
-import { RoomsModule } from './rooms/rooms.module';
 import { ChatModule } from './chat/chat.module';
 @Module({
   imports: [
@@ -11,7 +10,6 @@ import { ChatModule } from './chat/chat.module';
     AuthModule,
     PrismaModule,
     UsersModule,
-    RoomsModule,
     ChatModule,
   ],
   controllers: [],
--- backend/src/rooms/dto/create-room.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsNotEmpty, IsString } from 'class-validator';
-
-export class CreateRoomDto {
-  @IsNotEmpty()
-  @IsString()
-  name: string;
-}
